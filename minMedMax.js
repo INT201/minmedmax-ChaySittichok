4fb7dce12bf21b3c31f8fedda69a1f8dd74af4e6
ChaySittichok <sittichok.3345@mail.kmutt.ac.th>
2022-09-10 23:36:02 +0700

    edit file

--- minMedMax.js
+++ minMedMax.js
@@ -2,5 +2,17 @@ const { template } = require('@babel/core')
 
 function minMedMax(n1, n2, n3) {
   //code here
+  let array = [n1,n2,n3]
+for (let i = 0; i < array.length; i++) {
+  for (let x = i + 1; x < array.length; x++) {
+    if(array[i] > array[x]) {
+    let array0 ;
+    array0 = array[i];
+    array[i] = array[x];
+    array[x]= array0;
+  }
+ }
+}
+return {min:array[0], mid: array[1], max: array[2]}
 }
 module.exports = minMedMax
